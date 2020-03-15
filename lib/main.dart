e86e1813abf58d2479209505b95ef416bec835ee
Jailson Pereira dos Santos <xjailsonsantosx@info.ufrn.br>
2020-03-15 01:25:35 -0300

    adicionando botões de somar e subitrair com alinhamento

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,30 @@ void main() {
           Text('pesoas: 0',
               style:
                   TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  '+1',
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () => {},
+              ),
+              FlatButton(
+                child: Text(
+                  '-1',
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () => {},
+              ),
+            ],
+          ),
           Text('pode entrar',
-              style:
-                  TextStyle(color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30.0))
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0))
         ],
       )));
 }
