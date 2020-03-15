47cb4c062ba5a22d3a06d6db02591d8540cd54fa
Jailson Pereira dos Santos <xjailsonsantosx@info.ufrn.br>
2020-03-15 01:48:35 -0300

    adicionando layout stack e imagem de background

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,53 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: 'contador de pessoas',
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('pesoas: 0',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  '+1',
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+    title: 'contador de pessoas',
+    home: Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/original.jpg',
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('pesoas: 0',
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '+1',
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
-                onPressed: () => {},
-              ),
-              FlatButton(
-                child: Text(
-                  '-1',
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      '-1',
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
                 ),
-                onPressed: () => {},
-              ),
-            ],
-          ),
-          Text('pode entrar',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30.0))
-        ],
-      )));
+              ],
+            ),
+            Text('pode entrar',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0))
+          ],
+        )
+      ],
+    ),
+  ));
 }
